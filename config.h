5553bbfe247c8efcb10242cbe27bd484c1a540a3
robin ren <r.o.b.i.n@qq.com>
2019-11-27 12:44:19 +0800

    config

---
+++ config.h
@@ -0,0 +1,7 @@
+const char *ssid = "your wifi ssid";
+const char *passwd = "your wifi password";
+
+const char *id = "your device id";              //platform device id
+const char *apikey = "your device apikey";      //platform device api key
+const char *usrkey = "your user key";           //platform user key , if you are not using encrypted login,you can leave it blank
+const char *picId = "photo data stream id";     //photo data stream id
--- iot-tank.ino
+++ iot-tank.ino
@@ -10,13 +10,7 @@
 
 #define WIFI_TIMEOUT 30000
 
-const char *ssid = "your wifi ssid";
-const char *passwd = "your wifi password";
-
-const char *id = "your device id";              //platform device id
-const char *apikey = "your device apikey";      //platform device api key
-const char *usrkey = "your user key";           //platform user key , if you are not using encrypted login,you can leave it blank
-const char *picId = "photo data stream id";     //photo data stream id
+#include "config.h"
 
 BIGIOT bigiot;
 
