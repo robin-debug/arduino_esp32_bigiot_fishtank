36dff460ccc551a79021273ccf79a02336c30804
robin ren <r.o.b.i.n@qq.com>
2019-11-27 12:55:58 +0800

    read cam

--- iot-tank.ino
+++ iot-tank.ino
@@ -5,8 +5,13 @@
 #else
 #error "Only support espressif esp32/8266 chip"
 #endif
+
+#define DEBUG_BIGIOT_PORT Serial
 #include "bigiot.h"
-#include "girl.h"
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+#include "esp_camera.h"
 
 #define WIFI_TIMEOUT 30000
 
@@ -18,6 +23,28 @@ void eventCallback(const int devid, const int comid, const char *comstr, const c
 {
     // You can handle the commands issued by the platform here.
     Serial.printf(" device id:%d ,command id:%d command string:%s\n", devid, comid, comstr);
+    if(0==strncmp(comstr,"cam",3))
+    {
+        if(0==strcmp(comstr,"cam 2048")){
+            setCam(FRAMESIZE_QXGA);
+            uploadCam();         
+        }else if(0==strcmp(comstr,"cam 1600")){
+            setCam(FRAMESIZE_UXGA);
+            uploadCam();         
+        }else if(0==strcmp(comstr,"cam 1280")){
+            setCam(FRAMESIZE_SXGA);
+            uploadCam();         
+        }else if(0==strcmp(comstr,"cam 1024")){
+            setCam(FRAMESIZE_XGA);
+            uploadCam();         
+        }else if(0==strcmp(comstr,"cam 800")){
+            setCam(FRAMESIZE_SVGA);
+            uploadCam();         
+        }else /*if(0==strcmp(comstr,"cam 640"))*/{
+            setCam(FRAMESIZE_VGA);
+            uploadCam();         
+        }
+    }
 }
 
 void disconnectCallback(BIGIOT &obj)
@@ -34,18 +61,74 @@ void connectCallback(BIGIOT &obj)
     Serial.println("  connect");
 }
 
+void setCam(framesize_t framesize)
+{
+    sensor_t *s = esp_camera_sensor_get();
+    s->set_framesize(s, framesize);
+    //qqvga 160x120
+    //qvga  320x240
+    //vga   640x480
+    //svga  800x600
+    //xga   1024x768
+    //uxga  1600x1200
+}
+
+void initCam()
+{
+    Serial.println("init camera");
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    //init with high specs to pre-allocate larger buffers
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+      Serial.printf("Camera init failed with error 0x%x", err);
+      while (1);
+    }
+  
+    //drop down frame size for higher initial frame rate
+    setCam(FRAMESIZE_SVGA);
+}
+
 void setup()
 {
     Serial.begin(115200);
-
     delay(100);
 
+    initCam();
+
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
     WiFi.begin(ssid, passwd);
 
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
         Serial.print("Connect ssid fail");
         while (1);
     }
+    Serial.println("connected: OK");
 
     //Regist platform command event hander
     bigiot.eventAttach(eventCallback);
@@ -64,6 +147,24 @@ void setup()
     Serial.println("Connected to BIGIOT");
 }
 
+void uploadCam()
+{
+    Serial.println("cam");
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+        Serial.printf("Camera capture failed");
+        return;
+    }
+    Serial.printf("image len=%d,%d x %d\n", fb->len, fb->width, fb->height);
+    int len = fb->len;
+    if(len > 100 *1024 *1024)
+      len = 100 * 1024 * 1024;
+    if (!bigiot.uploadPhoto( picId, "jpg", "cam", (uint8_t *)fb->buf, len))
+        Serial.println("Upload error");
+    else
+        Serial.println("Upload Success");
+    esp_camera_fb_return(fb);    
+}
 
 void loop()
 {
@@ -87,18 +188,7 @@ void loop()
     //just uplaod once
     if (!isUpload) {
         isUpload = true;
-        Serial.print("Send girl photo to platform");
-
-        /*
-        const char *id          stream id
-        const char *type        photo type ,jpg,jpeg,png,gif
-        const char *filename    file name
-        uint8_t *image          array address
-        size_t size  array      size
-        */
-        if (!bigiot.uploadPhoto( picId, "jpg", "gril", (uint8_t *)girlArray, PIC_ARRAY_SIZE)) {
-            Serial.println("Upload error");
-        } else
-            Serial.println("Upload Success");
+        //uploadImg();
+        uploadCam();
     }
 }
