39252e76281efa56c7cd300548b4795bd72c98ad
robin ren <r.o.b.i.n@qq.com>
2019-11-30 20:45:06 +0800

    value

--- iot-tank.ino
+++ iot-tank.ino
@@ -25,33 +25,38 @@ const long  gmtOffset_sec = 8 * 3600;
 const int   daylightOffset_sec = 8 * 3600;
 
 const int   LED_BUILTIN = 4;
-//const int   PIN_OUTPUTA = 12;//noot boot,flash read err, 1000
+//const int   PIN_OUTPUTA = 12;//not boot,flash read err, 1000
 const int   PIN_OUTPUTA = 2;
 const int   PIN_OUTPUTB = 13;
 
 BIGIOT bigiot;
 
-void eventCallback(const int devid, const int comid, const char *comstr, const char *slave)
+void cmdValue(const char *client, const char *comstr)
 {
-    // You can handle the commands issued by the platform here.
-    Serial.printf(" device id:%d ,command id:%d command string:%s ,slave:%s\n", devid, comid, comstr, slave);
-    if(comid == PLAY)
-        comstr = "C1";//turn light on
-    else if(comid == STOP)
-        comstr = "C0";//trun light off
-    else if(comid == UP)
-        comstr = "cam";//take photo
+    String msg="cmdValue ";
+    String n=String(digitalRead(PIN_OUTPUTA));
+    bigiot.upload(&valueIds[0][0],n.c_str());
+    msg+=&valueIds[0][0];
+    msg+=n+" ";
+    n=String(digitalRead(PIN_OUTPUTB));
+    bigiot.upload(&valueIds[1][0],n.c_str());
+    msg+=&valueIds[1][0];
+    msg+=n+" ";
+    n=String(digitalRead(LED_BUILTIN));
+    bigiot.upload(&valueIds[2][0],n.c_str());
+    msg+=&valueIds[2][0];
+    msg+=n+" ";
+    Serial.println(msg.c_str());
+    if(client)
+        bigiot.sayToClient(client,msg.c_str());
+}
 
-    if(0==strncmp(comstr,"cam",3))
-        cmdCam(slave, comstr+3);
-    else if(0==strncmp(comstr,"time",4))
-        cmdTime(slave, comstr+4);
-    else if(0==strncmp(comstr,"light",5))
-        cmdLight(slave, comstr+5);
-    else if(0==strncmp(comstr,"task",4))
-        cmdTask(slave, comstr+4);
-    else
-        cmdHelp(slave, comstr);
+void cmdReboot(const char *client, const char *comstr)
+{
+    Serial.println("cmdReboot");
+    if(client)
+        bigiot.sayToClient(client,"reboot now");
+    esp_restart();
 }
 
 void cmdTask(const char *client, const char *comstr)
@@ -63,27 +68,36 @@ void cmdTask(const char *client, const char *comstr)
     else if(comstr[0]=='-')
         tasks.delTask(String(comstr+1));
     Serial.println(tasks.task.c_str());
-    bigiot.sayToClient(client,tasks.task.c_str());
+    if(client)
+        bigiot.sayToClient(client,tasks.task.c_str());
 }
 
 void cmdCam(const char *client, const char *comstr)
 {
     Serial.print("cmdCam ");
     Serial.println(comstr);
-    framesize_t framesize = FRAMESIZE_VGA;
+    framesize_t framesize = FRAMESIZE_SVGA;
     if(0==strcmp(comstr,"2048"))
-        framesize = FRAMESIZE_QXGA;//2040x1920
+        framesize = FRAMESIZE_QXGA;//2048*1536
     else if(0==strcmp(comstr,"1600"))
         framesize = FRAMESIZE_UXGA;//1600x1200
     else if(0==strcmp(comstr,"1280"))
-        framesize = FRAMESIZE_SXGA;//1280x960
+        framesize = FRAMESIZE_SXGA;//1280x1024
     else if(0==strcmp(comstr,"1024"))
         framesize = FRAMESIZE_XGA;//1024x768
     else if(0==strcmp(comstr,"800"))
         framesize = FRAMESIZE_SVGA;//800x600
+    else if(0==strcmp(comstr,"640"))
+        framesize = FRAMESIZE_VGA;//640x480    
     setCam(framesize);
-    uploadCam();         
-    bigiot.sayToClient(client,"photo upload!");
+    if(0==strcmp(comstr,"3")){
+        uploadCam();
+        uploadCam();
+        uploadCam();
+    }else
+        uploadCam();
+    if(client)       
+        bigiot.sayToClient(client,"photo upload!");
 }
 
 void cmdTime(const char *client, const char *comstr)
@@ -149,23 +163,54 @@ void cmdLight(const char *client, const char *comstr)
 
 void cmdHelp(const char *client, const char *comstr)
 {
-    String msg = String("bad cmd ") + comstr + ",try cam or time or light or task";
+    if(!client)
+        return;
+    String msg = String("bad cmd ") + comstr + ",try cam/time/light/task/value/reboot";
     bigiot.sayToClient(client,msg.c_str());
 }
 
-void disconnectCallback(BIGIOT &obj)
+
+void eventCallback(const int devid, const int comid, const char *comstr, const char *slave)
 {
-    // When the device is disconnected to the platform, you can handle your peripherals here
-    Serial.print(obj.deviceName());
-    Serial.println("  disconnect");
+    // You can handle the commands issued by the platform here.
+    Serial.printf(" device id:%d ,command id:%d command string:%s ,slave:%s\n", devid, comid, comstr, slave);
+    if(comid == PLAY)
+        comstr = "lightC1";//turn light on
+    else if(comid == STOP)
+        comstr = "lightC0";//trun light off
+    else if(comid == UP)
+        comstr = "cam";//take photo
+
+    if(0==strncmp(comstr,"cam",3))
+        cmdCam(slave, comstr+3);
+    else if(0==strncmp(comstr,"time",4))
+        cmdTime(slave, comstr+4);
+    else if(0==strncmp(comstr,"light",5)){
+        cmdLight(slave, comstr+5);
+        cmdValue(slave,"");//upload values
+    }else if(0==strncmp(comstr,"task",4))
+        cmdTask(slave, comstr+4);
+    else if(0==strncmp(comstr,"reboot",6))
+        cmdReboot(slave, comstr+6);
+    else if(0==strncmp(comstr,"value",5))
+        cmdValue(slave,comstr+5);
+    else
+        cmdHelp(slave, comstr);
 }
 
-void connectCallback(BIGIOT &obj)
-{
-    // When the device is connected to the platform, you can preprocess your peripherals here
-    Serial.print(obj.deviceName());
-    Serial.println("  connect");
-}
+//void disconnectCallback(BIGIOT &obj)
+//{
+//    // When the device is disconnected to the platform, you can handle your peripherals here
+//    Serial.print(obj.deviceName());
+//    Serial.println("  disconnect");
+//}
+//
+//void connectCallback(BIGIOT &obj)
+//{
+//    // When the device is connected to the platform, you can preprocess your peripherals here
+//    Serial.print(obj.deviceName());
+//    Serial.println("  connect");
+//}
 
 void setCam(framesize_t framesize)
 {
@@ -205,8 +250,8 @@ void initCam()
     // camera init
     esp_err_t err = esp_camera_init(&config);
     if (err != ESP_OK) {
-      Serial.printf("Camera init failed with error 0x%x", err);
-      while (1);
+        Serial.printf("Camera init failed with error 0x%x", err);
+        while (1);
     }
   
     //drop down frame size for higher initial frame rate
@@ -221,12 +266,12 @@ void setup()
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(PIN_OUTPUTA, OUTPUT);
     pinMode(PIN_OUTPUTB, OUTPUT);
-    cmdLight(NULL,"C1");
+    cmdLight(NULL,"C9");//led on
     digitalWrite(PIN_OUTPUTA,HIGH);//relay off
     digitalWrite(PIN_OUTPUTB,HIGH);//relay off
-    
-    initCam();
 
+    initCam();
+    
     Serial.print("Connecting to ");
     Serial.println(ssid);
     WiFi.begin(ssid, passwd);
@@ -240,20 +285,17 @@ void setup()
         cmdLight(NULL,"C9");
         Serial.print(".");
     }
-    cmdLight(NULL,"C1");
+    //cmdLight(NULL,"C9");
     Serial.println("connected: OK");
-
-    //init and get the time
-    cmdTime(NULL,"0");
 
     //Regist platform command event hander
     bigiot.eventAttach(eventCallback);
 
-    //Regist device disconnect hander
-    bigiot.disconnectAttack(disconnectCallback);
-
-    //Regist device connect hander
-    bigiot.connectAttack(connectCallback);
+//    //Regist device disconnect hander
+//    bigiot.disconnectAttack(disconnectCallback);
+//
+//    //Regist device connect hander
+//    bigiot.connectAttack(connectCallback);
 
     // Login to bigiot.net
     if (!bigiot.login(id, apikey, usrkey)) {
@@ -261,7 +303,11 @@ void setup()
         while (1);
     }
     Serial.println("Connected to BIGIOT");
-    cmdLight(NULL,"C0");
+
+    //init and get the time
+    cmdTime(NULL,"0");
+    
+    cmdLight(NULL,"C9");
 }
 
 void uploadCam()
@@ -309,20 +355,13 @@ void loop()
     } 
 
     {
-        //upload image every 60s
+        //upload image every 300s
         static long lastTime = 0;
         long now = millis();
-        if(lastTime == 0 || now - lastTime > 60000) {
+        if(lastTime == 0 || now - lastTime > 60 * 1000) {
             uploadCam();
-            lastTime = now;
-        }
-    }
-
-    {
-        static long lastTime = 0;
-        long now = millis();
-        if(lastTime == 0 || now - lastTime > 10000) {
-            cmdTime(NULL,"time");
+            cmdValue(NULL,"");
+            //cmdTime(NULL,"");
             lastTime = now;
         }
     }
